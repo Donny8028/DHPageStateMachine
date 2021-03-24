2f9b4621e770179bae68f81674a588d6cc2c846d
DonnyHoo <donnyho@accuvally.com>
2021-03-24 18:29:19 +0800

    fix: make configuration public init

--- Sources/DHPageStateMachine/DHPageStateAPIWorkerConfiguration.swift
+++ Sources/DHPageStateMachine/DHPageStateAPIWorkerConfiguration.swift
@@ -8,11 +8,16 @@
 import Foundation
 
 public struct DHPageStateAPIWorkerConfiguration {
-    enum PageBase {
+    public enum PageBase {
         case zero, one
     }
     let oneTimeLoad: Bool
     let pageBase: PageBase
 
     public static let `default`: DHPageStateAPIWorkerConfiguration = .init(oneTimeLoad: false, pageBase: .zero)
+
+    public init(oneTimeLoad: Bool, pageBase: DHPageStateAPIWorkerConfiguration.PageBase) {
+        self.oneTimeLoad = oneTimeLoad
+        self.pageBase = pageBase
+    }
 }
